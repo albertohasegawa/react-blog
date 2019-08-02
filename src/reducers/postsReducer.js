c4d12bc6752072d356feb5cc8308162789bb3080
albertohasegawa <albertohasegawa@gmail.com>
2019-08-02 12:31:13 -0300

    aula 185

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,12 @@
-export default () => {
-  return 123;
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
+
+  //bad return document.querySelector('input');
+  //bad return axios.get('/posts');
+  //good return state + action
 };
